82a25e7072593d67caf2c06c9bfb74eb66a43a00
The Lumberjack <thelumberjack@TheLumberjacks-laptop.(none)>
2009-09-24 17:21:56 -0400

    added new packages

---
+++ Lab 1 Soft Eng/design3/AbstractPoint.java
@@ -0,0 +1,63 @@
+package design3;
+
+public abstract class AbstractPoint
+{
+	public static final int CARTESIAN = 0;
+	public static final int POLAR = 1;
+	int typeCoord;
+	double xOrRho;
+	double yOrTheta;
+	public AbstractPoint(int typeCoord, double xOrRho, double yOrTheta)
+	{
+		if (typeCoord != design3.AbstractPoint.CARTESIAN || typeCoord != design3.AbstractPoint.POLAR)
+			throw new IllegalArgumentException();
+		this.typeCoord = typeCoord;
+		this.xOrRho = xOrRho;
+		this.yOrTheta = yOrTheta;
+	}
+	public double getX()
+	{
+		if(typeCoord == design3.AbstractPoint.CARTESIAN) 
+			return xOrRho;
+		else 
+			return (Math.cos(Math.toRadians(yOrTheta)) * xOrRho);
+	}
+	
+	public double getY()
+	{
+		if(typeCoord == design3.AbstractPoint.CARTESIAN) 
+			return yOrTheta;
+		else 
+			return (Math.sin(Math.toRadians(yOrTheta)) * xOrRho);
+	}
+
+	public double getRho()
+	{
+		if(typeCoord == design3.AbstractPoint.POLAR) 
+			return xOrRho;
+		else 
+			return (Math.sqrt(Math.pow(xOrRho, 2) + Math.pow(yOrTheta, 2)));
+	}
+
+	public double getTheta()
+	{
+		if(typeCoord == design3.AbstractPoint.POLAR)
+			return yOrTheta;
+		else 
+			return Math.toDegrees(Math.atan2(yOrTheta, xOrRho));
+	}
+	
+	public double getDistance(PointCP pointB)
+	{
+		// Obtain differences in X and Y, sign is not important as these values
+		// will be squared later.
+		double deltaX = getX() - pointB.getX();
+		double deltaY = getY() - pointB.getY();
+	 
+		return Math.sqrt((Math.pow(deltaX, 2) + Math.pow(deltaY, 2)));
+	}
+	
+	public abstract AbstractPoint rotatePoint(double rotation);
+	
+	public abstract String toString();
+}
