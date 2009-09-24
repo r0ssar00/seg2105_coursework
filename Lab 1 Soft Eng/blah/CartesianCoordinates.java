1e439e67923f4cf3a5b6153ba0791118e034202a
Kevin Ross <r0ssar00@gmail.com>
2009-09-23 22:43:26 -0400

    design 3 dev'ed

---
+++ Lab 1 Soft Eng/blah/CartesianCoordinates.java
@@ -0,0 +1,9 @@
+package blah;
+
+public class CartesianCoordinates extends PointCP {
+	public CartesianCoordinates(double X, double Y) {
+		super('C', X, Y);
+	}
+	public void convertStorageToPolar() {}
+	public void convertStorageToCartesian() {}
+}
